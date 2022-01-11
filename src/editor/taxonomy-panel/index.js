e34ba2e43866d8ec93c7fb2a332229d9d8ef4dab
Derrick Koo <derrick.koo@automattic.com>
2022-01-11 16:45:45 -0700

    fix(wp5.9): taxonomy panel filter no longer includes taxonomy data in filtered props

--- src/editor/taxonomy-panel/index.js
+++ src/editor/taxonomy-panel/index.js
@@ -20,8 +20,12 @@ export const TaxonomyPanel = PostTaxonomies => {
 			return <PostTaxonomies { ...props } />;
 		}
 
-		const { slug, taxonomy } = props;
-		const { hierarchical, labels } = taxonomy;
+		const { slug } = props;
+		const hierarchical = 'category' === slug;
+		const label =
+			'category' === slug
+				? __( 'categories', 'newspack-sponsors' )
+				: __( 'tags', 'newspack-sponsors' );
 		const message = sprintf(
 			// Translators: explanation for applying sponsors to a taxonomy term.
 			__(
@@ -30,8 +34,8 @@ export const TaxonomyPanel = PostTaxonomies => {
 			),
 			// Translators: "Select" terms if none added yet, or "Add" terms if there's at least one selected already.
 			hierarchical ? __( 'Select ', 'newspack-sponsors' ) : __( 'Add ', 'newspack-sponsors' ),
-			labels.name.toLowerCase(),
-			labels.name.toLowerCase()
+			label,
+			label
 		);
 
 		return (
